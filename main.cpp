b22c6733698aba3112446fa79248a80c6ea91c44
John Eleanor <mrs.ch64@gmail.com>
2023-10-07 20:05:25 +0700

    Clean File

--- main.cpp
+++ main.cpp
@@ -8,12 +8,12 @@ using namespace std;
 
 int main()
 {
-    //![📂] About File
+    //?[📂] About File
     string Filename = "user.txt";
     ifstream InFile;
     ofstream OutFile;
 
-    //![📂] About User
+    //?[📂] About User
     bool CheckUserStatus;
     string username, password;
 
@@ -27,7 +27,7 @@ int main()
         cin >> password;
         cout << setfill('=') << setw(55) << "=" << endl;
 
-        InFile.open(Filename.c_str()); //! เปิดไฟล์เเละเช็ค
+        InFile.open(Filename.c_str());  //! เปิดไฟล์เเละเช็ค
         CheckUserStatus = CheckUser(InFile, username, password);
         InFile.close();
 
@@ -38,45 +38,48 @@ int main()
 
     } while (CheckUserStatus == false);
 
-    // If ststus login Pass Do The 
-    if (CheckUserStatus == true)
+
+    if (CheckUserStatus == true)    //? 🟢] ถ้า Login ผ่านจึงจะทำงานตาม Code ด้านล่าง
     {
-        // int choice_num_set = 2; //! [🧠]Setting จำนวนเมนู
-        string select_menu;
+        string select_menu;         //? [🧠] สำหรับเงื่อนไขเมนู
 
         do
         {
 
-            main_menu(); //! MENU
+            main_menu(); //? MENU
             cin >> select_menu;
 
-            
-            if (select_menu == "1") // เกี่ยวกับระบบห้อง 
+            if (select_menu == "1") // เกี่ยวกับระบบห้อง
             {
 
                 int room_select;
                 do
                 {
-                    room_menu(); //! MENU
+                    room_menu(); //? MENU
                     cin >> room_select;
 
                     if (room_select == 0)
                     {
                         select_menu = "0";
                         break;
-                    }else if ( room_select == 1 ) {
+                    }
+                    else if (room_select == 1)
+                    {
 
                         cout << "This  is Menu 1 : \n";
-
-                    }else if ( room_select == 2 ){
+                    }
+                    else if (room_select == 2)
+                    {
 
                         cout << "this is menu 2 : \n";
-
-                    }else if ( room_select  == 3) {
+                    }
+                    else if (room_select == 3)
+                    {
 
                         cout << "This is Menu 3 : \n";
-
-                    }else {
+                    }
+                    else
+                    {
                         cout << "Menu is not found ";
                     }
 
@@ -95,34 +98,41 @@ int main()
                     {
                         select_menu = "0";
                         break;
-                    }else if (user_select == 1){
+                    }
+                    else if (user_select == 1)
+                    {
 
                         cout << "this is menu 1 : \n";
-
-                    }else if (user_select == 2){
+                    }
+                    else if (user_select == 2)
+                    {
 
                         cout << "This is menu 2 : \n";
-
-                    }else if ( user_select = 3 ) {
+                    }
+                    else if (user_select = 3)
+                    {
 
                         cout << "This is menu 3 : \n";
-
-                    }else {
+                    }
+                    else
+                    {
                         cout << "Menu is not found : \n";
                     }
 
                 } while (user_select != 9 && (user_select <= 4));
-            }else {
+            }
+            else
+            {
                 cout << "Plase Try Again \n";
             }
 
-        } while ( select_menu != "0" );
-
+        } while (select_menu != "0");
     }
     else
     {
         cout << "Login Failed ";
     }
+
     cout << setfill('=') << setw(55) << "=" << endl;
     cout << "Thank You For :)";
 

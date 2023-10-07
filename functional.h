f3ad474eadb39e5db986dd5989f1c908c0f1288d
John Eleanor <mrs.ch64@gmail.com>
2023-10-07 14:34:03 +0700

    Add Header File

---
+++ functional.h
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+/*
+    [🧠] หน้าเมนูหลัก 
+    [📂] ใช้สำหรับ : เเสดงผลเท่านั้น
+*/
+void main_menu()
+{
+    system("CLS");
+    cout << "===== Welcome =====" << endl;
+    cout << "[1] Room management \n";
+    cout << "[2] User management \n";
+    cout << "[0] EXIT Program \n";
+    cout << "===================\n";
+    cout << "[+] Plase Enter Your Choice : ";
+}
+
+/*
+    [🧠] หน้าเมนูจัดการห้อง
+    [📂] ใช้สำหรับ : เเสดงผลเท่านั้น
+*/
+void room_menu()
+{
+    system("CLS");
+    cout << "===== Room management =====\n";
+    cout << "[1] Create Room \n";
+    cout << "[2] Edit Room \n";
+    cout << "[3] Delete Room \n";
+    cout << "[9] Exit Menu \n";
+    cout << "[0] Exit Program \n";
+    cout << "===================\n";
+    cout << "[+] Plase Enter Your Choice : ";
+}
+
+/*
+    [🧠] หน้าเมนูจัดการผู้ใช้
+    [📂] ใช้สำหรับ : เเสดงผลเท่านั้น
+*/
+void user_menu()
+{
+    system("CLS");
+    cout << "===== User management =====\n";
+    cout << "[1] Add User \n";
+    cout << "[2] Edit User \n";
+    cout << "[3] Delete User \n";
+    cout << "[9] Exit Menu \n";
+    cout << "[0] Exit Program \n";
+    cout << "===================\n";
+    cout << "[+] Plase Enter Your Choice : ";
+}
+
+/*
+    [🧠] เพิ่มผู้ใชงาน
+    [📂] ใช้สำหรับ : เพิ่มผู้ใช้งาน
+*/
+void InsertData(ofstream &OutFile)
+{
+    string Id, Name, SurName;
+    cout << endl;
+}
+
+/*
+    [🧠] เช็อการ Login
+    [📂] ใช้สำหรับ : เช็คผู้ใชงานสำหรับ Login
+*/
+bool CheckUser(ifstream &InFile, string param_username, string param_password)
+{
+
+    string username, pass;
+
+    while (InFile >> username >> pass)
+    {
+        if (param_username == username && param_password == pass)
+        {
+            cout << "Login Successfully" << endl;
+            return true;
+        }
+    }
+
+            
+    return false;
+
+}
--- main.cpp
+++ main.cpp
@@ -2,24 +2,9 @@
 #include <string>
 #include <fstream>
 #include <iomanip>
+#include "functional.h"
 
 using namespace std;
-
-
-// เช็คผู้ใช้งาน 
-bool CheckUser(ifstream &InFile, string param_username, string param_password);
-
-/*
- * [🧠] MENU ZONE
- */
-void main_menu();
-void room_menu();
-
-/*
- * [🧠] ROOM ZONE
- */
-void InsertData(ofstream &OutFile);
-
 
 int main()
 {
@@ -30,19 +15,30 @@ int main()
     bool CheckUserStatus;
 
     string username, password;
-    cout << setfill('*') << setw(55) << "*" << endl;
+
+
+
+    do
+    {
+            
+    cout << setfill('=') << setw(55) << "=" << endl;
     cout << setfill(' ') << setw(25) <<  "Username : ";
     cin >> username;
     cout << setfill(' ') << setw(25) << "Password : ";
     cin >> password;
-    cout << setfill('*') << setw(55) << "*" << endl;
+    cout << setfill('=') << setw(55) << "=" << endl;
 
-    /*
-        ? เปิดไฟล์เเละเช็ค
-    */
-    InFile.open(Filename.c_str());
+
+    InFile.open(Filename.c_str()); //! เปิดไฟล์เเละเช็ค
     CheckUserStatus = CheckUser(InFile, username, password);
     InFile.close();
+
+    if(CheckUserStatus == false ){
+        cout << "Login Failed Try Again\n" ;
+    }
+
+    } while(CheckUserStatus == false);
+
 
     if (CheckUserStatus == true)
     {
@@ -54,6 +50,7 @@ int main()
 
             main_menu(); //! MENU
             cin >> select_menu;
+            
 
             if (select_menu == 1)
             {
@@ -71,62 +68,31 @@ int main()
                     }
 
                 } while (room_select != 9 && (room_select <= 4));
+
+            }else if(select_menu == 2){
+
+                int user_select;
+                do
+                {
+                    user_menu(); //! MENU
+                    cin >> user_select;
+
+                    if (user_select == 0)
+                    {
+                        select_menu = 0;
+                        break;
+                    }
+
+                } while (user_select != 9 && (user_select <= 4));
+
             }
 
         } while (select_menu != 0 && select_menu <= choice_num_set);
     }else {
         cout << "Login Failed ";
     }
-
+    cout << setfill('=') << setw(55) << "=" << endl;
+    cout << "Thank You For :)";
+    
     return 0;
-}
-
-void main_menu()
-{
-    system("CLS");
-    cout << "===== Welcome =====" << endl;
-    cout << "[1] Room management \n";
-    cout << "[2] User management \n";
-    cout << "[0] EXIT Program \n";
-    cout << "===================\n";
-    cout << "[+] Plase Enter Your Choice : ";
-}
-
-void room_menu()
-{
-    system("CLS");
-    cout << "===== Room management =====\n";
-    cout << "[1] Create Room \n";
-    cout << "[2] Edit Room \n";
-    cout << "[3] Delete Room \n";
-    cout << "[9] Exit Menu \n";
-    cout << "[0] Exit Program \n";
-    cout << "===================\n";
-    cout << "[+] Plase Enter Your Choice : ";
-}
-
-void InsertData(ofstream &OutFile)
-{
-    string Id, Name, SurName;
-    cout << endl;
-}
-
-
-bool CheckUser(ifstream &InFile, string param_username, string param_password)
-{
-
-    string username, pass;
-
-    while (InFile >> username >> pass)
-    {
-        if (param_username == username && param_password == pass)
-        {
-            cout << "Login Successfully" << endl;
-            return true;
-        }
-    }
-
-            
-    return false;
-
 }
